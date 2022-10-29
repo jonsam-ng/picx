a7e2bee7b44ef54318c9bc40cf9ade512430f24a
jonsam <jonsam.ng@foxmail.com>
2022-10-29 21:59:42 +0800

    feat: 🆕 support html gif

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -25,3 +25,60 @@ export const getLocalItem = (key: string) => {
   const temp = window.localStorage.getItem(key)
   return temp ? JSON.parse(temp) : null
 }
+
+export const getExtensionFromUrl = (url) => url.match(/\.([^./?]+)($|\?)/)[1]
+
+export const getUrlsFromHtml = (html) => {
+  const urls = []
+  const pattern = /<img[^>]+src="?([^"\s]+)"?.*\/?>/g
+  let m = pattern.exec(html)
+  // try max times at most, also avoid dead loop
+  let max = 5
+  while (m && max > 0) {
+    if (m[1]) {
+      max -= 1
+      urls.push(m[1])
+    }
+    m = pattern.exec(html)
+  }
+  return urls
+}
+
+export const isValidUrl = (urlString) => {
+  const urlPattern = new RegExp(
+    '^(https?:\\/\\/)?' + // validate protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // validate fragment locator
+  return !!urlPattern.test(urlString)
+}
+
+export const readFileFromUrl = (url) =>
+  new Promise((resolve) => {
+    if (!url || !isValidUrl(url)) resolve(null)
+    try {
+      // Opaque responses can't be accessed with JavaScript
+      // see https://stackoverflow.com/questions/46641508/download-image-using-fetch-api
+      fetch(url, {
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'Access-Control-Allow-Origin': '*'
+        },
+        mode: 'no-cors'
+      })
+        .then((res) => res.blob())
+        .then((blob) => {
+          const ext = getExtensionFromUrl(url)
+          const file = new File([blob], `image.${ext}`, {
+            type: `image/${ext}`
+          })
+          resolve(file)
+        })
+    } catch (error) {
+      resolve(null)
+    }
+  })
--- src/utils/paste.ts
+++ src/utils/paste.ts
@@ -1,4 +1,5 @@
 import selectedFileHandle from './selected-file-handle'
+import { isValidUrl, readFileFromUrl, getUrlsFromHtml } from './common-utils'
 
 const onPaste = (e: any, maxsize: number): Promise<any> | null => {
   if (!(e.clipboardData && e.clipboardData.items)) {
@@ -19,6 +20,31 @@ const onPaste = (e: any, maxsize: number): Promise<any> | null => {
           }
           const { base64, originalFile, compressFile } = result
           resolve({ base64, originalFile, compressFile })
+        })
+      } else if (item.kind === 'string') {
+        // extract image from html like gif
+        item.getAsString((pasteString) => {
+          if (
+            !pasteString ||
+            !['<img', 'src='].every((s) => pasteString.indexOf(s) !== -1)
+          )
+            return
+          // extract urls
+          const urls = getUrlsFromHtml(pasteString)
+
+          if (!urls || !urls.length) return
+          urls.forEach(async (url) => {
+            const file = await readFileFromUrl(url)
+            console.log('==>', { file })
+            if (!file) return
+            // handle this file
+            selectedFileHandle(file, maxsize)?.then((result) => {
+              console.log('==>', { result })
+              if (!result) return
+              const { base64, originalFile, compressFile } = result
+              resolve({ base64, originalFile, compressFile })
+            })
+          })
         })
       }
     }
